ff692d71dc06719fdde60c7be450bceb487b9763
CodeBlack0 <tim.fischer98@hotmail.com>
2016-04-24 15:50:16 +0200

    added fill_area to PNMDraw

--- Ruby/obj/pnm_draw.rb
+++ Ruby/obj/pnm_draw.rb
@@ -8,11 +8,11 @@ class PNMDraw
   end
 
   def line(canvas, pt1, pt2, opt={})
-  	color = opt[:color] || opt[:c]
-  	color = canvas.type == :ppm ? [canvas.maxgray, canvas.maxgray, canvas.maxgray] : canvas.type == :pgm ? canvas.maxgray : 1 unless opt.has_key?(:color) || opt.has_key?(:c)
+    color = opt[:color] || opt[:c]
+    color = canvas.type == :ppm ? [canvas.maxgray, canvas.maxgray, canvas.maxgray] : canvas.type == :pgm ? canvas.maxgray : 1 unless opt.has_key?(:color) || opt.has_key?(:c)
 
-  	@generator.parse_options({:h => canvas.height, :w => canvas.width, :d => 50})
-    line = @generator.line(pt1[0], pt1[1], pt2[0], pt2[1]).pixels.dup  	
+    @generator.parse_options({:h => canvas.height, :w => canvas.width, :d => 50})
+    line = @generator.line(pt1[0], pt1[1], pt2[0], pt2[1]).pixels.dup
     original = canvas.pixels.dup
     
     edited = original.map.with_index { |row, y| row.map.with_index { |pixel, x| (line[y][x] == 1) ? color : pixel}}
@@ -20,11 +20,11 @@ class PNMDraw
   end
 
   def pbm_onto_image(canvas, map, opt={})
-  	return "map not pbm" if map.type != :pbm
-  	color = opt[:color] || opt[:c]
-  	color = canvas.type == :ppm ? [canvas.maxgray, canvas.maxgray, canvas.maxgray] : canvas.type == :pgm ? canvas.maxgray : 1 unless opt.has_key?(:color) || opt.has_key?(:c)
+    return "map not pbm" if map.type != :pbm
+    color = opt[:color] || opt[:c]
+    color = canvas.type == :ppm ? [canvas.maxgray, canvas.maxgray, canvas.maxgray] : canvas.type == :pgm ? canvas.maxgray : 1 unless opt.has_key?(:color) || opt.has_key?(:c)
     
-    bwmap = map.pixels.dup  	
+    bwmap = map.pixels.dup
     original = canvas.pixels.dup
 
     edited = original.map.with_index { |row, y| row.map.with_index { |pixel, x| (bwmap[y][x] == 1) ? color : pixel}}
@@ -44,11 +44,11 @@ class PNMDraw
     
     final = []
     canvas.height.times do |y|
-    	row = []
+      row = []
       canvas.width.times do |x|
         pixel = canvas.pixels[y][x]
-      	if (sum_a.has_key?(x) && sum_c.has_key?(x)) && !(y >= sum_a[x] || y <= sum_c[x])
-        	pixel = color
+        if (sum_a.has_key?(x) && sum_c.has_key?(x)) && !(y >= sum_a[x] || y <= sum_c[x])
+          pixel = color
         end
         row << pixel
       end
